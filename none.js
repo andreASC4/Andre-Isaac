725c9c87d517714388dbc9f8af8df33dcff32a76
andreASC4 <aisaac2020@bklawtech.com>
2017-07-11 10:32:24 -0700

    first commit

---
+++ none.js
@@ -0,0 +1,18 @@
+var myPokemonRoster = ["Blazikin","Lucario","Deoxsis","Darkrai","Giratina"];
+
+var firstToFight = "Blazikin";
+var secondToFight = "Lucario";
+
+//myPokemonRoster.pop();
+
+myPokemonRoster.push("Alakazam","Snorlax");
+
+//myPokemonRoster.length;
+
+//console.log(myPokemonRoster);
+
+var i = 0;
+for (i=0; i<5; i++){
+console.log(myPokemonRoster[i]);
+}
+
---
+++ of.js
@@ -0,0 +1,4 @@
+for (i=0; i<5; i++){
+    var myArray = ["bob","bill","burnes","ben","bin"];
+    console.log(myArray[i]);
+}
